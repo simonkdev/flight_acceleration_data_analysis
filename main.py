7d9e81f3891f307c903c6e38bb598fd6ca4cbec8
simonkdev <simonkorten09@gmail.com>
2026-07-06 14:21:35 +0200

    Implemented window smoothing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-SMOOTHING_INCREMENT = 5
+ROLLING_WINDOW = 12
 
 from time import time
 
@@ -32,70 +32,87 @@ printInit("Data loaded successfully.")
 printInfo("Seaborn version: " + sns.__version__)
 printInit("Setting up Seaborn...")
 sns.set_theme()
+
+def grid_plot_one_row(dataframe, linePlot = False):
+    fig, axes = plt.subplots(1,4, figsize=(15, 4), sharex=True)
+
+    if linePlot:
+        sns.lineplot(data=dataframe, x="time", y="acc_x", ax=axes[0])
+    else:
+        sns.scatterplot(data=dataframe, x="time", y="acc_x", ax=axes[0])
+    axes[0].set_title("X acceleration")
+
+    if linePlot:
+        sns.lineplot(data=dataframe, x="time", y="acc_y", ax=axes[1])
+    else:
+        sns.scatterplot(data=dataframe, x="time", y="acc_y", ax=axes[1])
+    axes[1].set_title("Y acceleration")
+
+    if linePlot:
+        sns.lineplot(data=dataframe, x="time", y="acc_z", ax=axes[2])
+    else:
+        sns.scatterplot(data=dataframe, x="time", y="acc_z", ax=axes[2])
+    axes[2].set_title("Z acceleration")
+
+    if linePlot:
+        sns.lineplot(data=dataframe, x="time", y="absolute_acc", ax=axes[3])
+    else:
+        sns.scatterplot(data=dataframe, x="time", y="absolute_acc", ax=axes[3])
+    axes[3].set_title("Absolute acceleration")
+
+    for ax in axes:
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Acceleration")
+
+    plt.tight_layout()
+
 printInit("Seaborn set up successfully.")
 
 printSection("Visualization")
+
 printStatus("Creating noisy scatter plots...")
 
-fig, axes = plt.subplots(1,4, figsize=(15, 4), sharex=True)
+grid_plot_one_row(df)
 
-sns.scatterplot(data=df, x="time", y="acc_x", ax=axes[0])
-axes[0].set_title("X acceleration")
-
-sns.scatterplot(data=df, x="time", y="acc_y", ax=axes[1])
-axes[1].set_title("Y acceleration")
-
-sns.scatterplot(data=df, x="time", y="acc_z", ax=axes[2])
-axes[2].set_title("Z acceleration")
-
-sns.scatterplot(data=df, x="time", y="absolute_acc", ax=axes[3])
-axes[3].set_title("Absolute acceleration")
-
-for ax in axes:
-    ax.set_xlabel("Time")
-    ax.set_ylabel("Acceleration")
-
-plt.tight_layout()
 printStatus("Saving noisy scatter plots...")
+
 plt.savefig("acceleration_scatterplots.png", dpi=300, bbox_inches="tight")
+
 printStatus("Noisy plots saved.")
 
 printSection("Smoothing")
 
+def smoothing(df_column, rows):
+    arr = np.full(ROLLING_WINDOW, np.nan)
+    tracker = ROLLING_WINDOW
+
+    with Progress() as p:
+        t = p.add_task(colored("[ STAT ]  ", "cyan") + "Calculating smoothed values...", total=rows-ROLLING_WINDOW)
+        while not p.finished:
+            p.update(t, advance=1)
+
+            smoothed_value = df_column.iloc[tracker-ROLLING_WINDOW:tracker].mean()
+
+
+            arr = np.append(arr, smoothed_value)
+            tracker += 1
+
+    return arr
+
 printStatus("Preparing smoothing...")
-arr = np.array([])
-time_arr = np.array([])
-tracker = 0
-
 rows = len(df)
-steps = rows // SMOOTHING_INCREMENT
+time = df["time"]
 
-with Progress() as p:
-    t = p.add_task(colored("[ STAT ]  ", "cyan") + "Calculating smoothed values...", total=steps)
-    while not p.finished:
-        p.update(t, advance=1)
-        if tracker + SMOOTHING_INCREMENT > rows:
-            mean_of_15 = df["acc_x"].iloc[tracker:].mean()
-            time_mean = df["time"].iloc[tracker:].mean()
-        else:
-            mean_of_15 = df["acc_x"].iloc[tracker:tracker+SMOOTHING_INCREMENT].mean()
-            time_mean = df["time"].iloc[tracker:tracker+SMOOTHING_INCREMENT].mean()
+acc_x, acc_y, acc_z, absolute_acc = df["acc_x"], df["acc_y"], df["acc_z"], df["absolute_acc"]
 
-        arr = np.append(arr, mean_of_15)
-        time_arr = np.append(time_arr, time_mean)
-        tracker += SMOOTHING_INCREMENT
-
-smoothed_df = pd.DataFrame({"time": time_arr, "acc_x": arr})
+smoothed_df = pd.DataFrame({"time": time, "acc_x": smoothing(acc_x, rows), "acc_y": smoothing(acc_y, rows), "acc_z": smoothing(acc_z, rows), "absolute_acc": smoothing(absolute_acc, rows)})
 
 smoothed_df.to_csv("smoothed_df.csv", index=False)
 
-plt.figure(figsize=(8, 4))
+printStatus("Creating smoothed plot...")
 
-sns.lineplot(data=smoothed_df, x="time", y="acc_x")
-plt.title("Smoothed acceleration")
-plt.xlabel("Time")
-plt.ylabel("Acceleration")
-plt.tight_layout()
+grid_plot_one_row(smoothed_df, True)
+
 printStatus("Saving smoothed plot...")
 plt.savefig("smoothed_acceleration.png", dpi=300, bbox_inches="tight")
 printStatus("Smoothed plot saved.")
